d5e71ff60f73e6672f2988a64a38b41bed9976fc
Pavel Oreshkin <poreskin@axbit.ru>
2018-11-04 00:11:07 +0400

    task6 is done

---
+++ task6 JS (request, promises)/index.js
@@ -0,0 +1,31 @@
+const getUsersApi = (name) => {
+  return fetch(`https://api.github.com/search/users?q=${name}`)
+    .then(res => {
+      return res.json();
+    })
+}
+
+const enter = (event) => {
+  if (event.key === 'Enter') {
+    getUsers()
+  }
+}
+
+const getUsers = () => {
+  const name = document.getElementById('textInput').value;
+  getUsersApi(name)
+    .then(res => {
+      document.getElementById('root').innerHTML = '';
+      const fiveUser = res.items.slice(0, 5);
+      fiveUser.forEach((item, index) => {
+        document.getElementById('root').innerHTML += `
+        <div class="resultUser">
+          <img src="${item.avatar_url}" alt="avatar" class="resultUser__avatar" />
+          <div class="resultUser__content">
+            <h1 class="resultUser__login">${item.login}</h1>
+            <a class="searchPanel__searchButton" target="_blank" href="${item.html_url}">Открыть репозиторий</a>
+          </div>          
+        </div>`;
+      })
+    })
+}

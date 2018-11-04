6a0fdaca1b21a1c6be7df456f6b1075fe8efe045
Pavel Oreshkin <poreskin@axbit.ru>
2018-11-04 16:39:36 +0400

    add error handler

--- task6 JS (request, promises)/index.js
+++ task6 JS (request, promises)/index.js
@@ -15,17 +15,22 @@ const getUsers = () => {
   const name = document.getElementById('textInput').value;
   getUsersApi(name)
     .then(res => {
-      document.getElementById('root').innerHTML = '';
-      const fiveUser = res.items.slice(0, 5);
-      fiveUser.forEach((item, index) => {
-        document.getElementById('root').innerHTML += `
-        <div class="resultUser">
-          <img src="${item.avatar_url}" alt="avatar" class="resultUser__avatar" />
-          <div class="resultUser__content">
-            <h1 class="resultUser__login">${item.login}</h1>
-            <a class="searchPanel__searchButton" target="_blank" href="${item.html_url}">Открыть репозиторий</a>
-          </div>          
-        </div>`;
-      })
+      if (res.items.length !== 0) {
+        document.getElementById('root').innerHTML = '';
+        const fiveUser = res.items.slice(0, 5);
+        fiveUser.forEach((item, index) => {
+          document.getElementById('root').innerHTML += `
+          <div class="resultUser">
+            <img src="${item.avatar_url}" alt="avatar" class="resultUser__avatar" />
+            <div class="resultUser__content">
+              <h1 class="resultUser__login">${item.login}</h1>
+              <a class="searchPanel__searchButton" target="_blank" href="${item.html_url}">Открыть репозиторий</a>
+            </div>          
+          </div>`;
+        })
+      } else {
+        alert('Пользователь с таким именем не найден');
+      }
     })
+    .catch(err => alert('Неизвестная ошибка обратитесь к администратору'))
 }

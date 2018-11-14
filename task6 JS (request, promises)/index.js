7d0b3d599f208c2e03f506b75470432366e8f94e
Pavel Oreshkin <poreskin@axbit.ru>
2018-11-14 23:12:36 +0400

    refactor code, add 2 seconds delay if push Enter button

--- task6 JS (request, promises)/index.js
+++ task6 JS (request, promises)/index.js
@@ -8,29 +8,34 @@ const getUsersApi = (name) => {
 const enter = (event) => {
   if (event.key === 'Enter') {
     getUsers()
+      .then(res => {
+        if (res.items) {
+          setTimeout(() => {
+            alert('Все получилось!)')
+          }, 2000);
+        }
+      })
   }
 }
 
 const getUsers = () => {
   const name = document.getElementById('textInput').value;
-  getUsersApi(name)
+  return getUsersApi(name)
     .then(res => {
       if (res.items.length !== 0) {
-        document.getElementById('root').innerHTML = '';
-        const fiveUser = res.items.slice(0, 5);
-        fiveUser.forEach((item, index) => {
-          document.getElementById('root').innerHTML += `
-          <div class="resultUser">
-            <img src="${item.avatar_url}" alt="avatar" class="resultUser__avatar" />
+        document.getElementById('root').innerHTML = res.items.slice(0, 5).map(item =>
+          `<div class="resultUser">
+            <img src="${(item.avatar_url) ? item.avatar_url : '#'}" alt="avatar" class="resultUser__avatar" />
             <div class="resultUser__content">
-              <h1 class="resultUser__login">${item.login}</h1>
-              <a class="searchPanel__searchButton" target="_blank" href="${item.html_url}">Открыть репозиторий</a>
+              <h1 class="resultUser__login">${(item.login) ? item.login : ''}</h1>
+              <a class="searchPanel__searchButton" target="_blank" href="${(item.html_url) ? item.html_url : ''}">Открыть репозиторий</a>
             </div>          
-          </div>`;
-        })
+          </div>`).join('');
       } else {
+        document.getElementById('root').innerHTML = '';
         alert('Пользователь с таким именем не найден');
       }
+      return res
     })
     .catch(err => alert('Неизвестная ошибка обратитесь к администратору'))
 }
